ac6fa999ed37c675add6d44c7934ae1cc01ecf17
yuvalbenar <yuvalbenar@gmail.com>
2024-11-12 10:52:10 +0200

    add in some files

---
+++ firstexrecise.py
@@ -0,0 +1,7 @@
+capitals= {
+  'France': 'Paris',
+  'Spain': 'Madrid',
+  'Japan': 'Tokyo'
+}
+x = capitals.get("germany")
+print(x)
---
+++ firstexreciseforloop.py
@@ -0,0 +1,8 @@
+capitals= {
+  'France': 'Paris',
+  'Spain': 'Madrid',
+  'Japan': 'Tokyo'
+}
+for x, y in capitals.items():
+
+  print("the capital of", x,"is", y)
