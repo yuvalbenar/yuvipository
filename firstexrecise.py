400218f87790efa0a33151cbf6c44208e2d307af
yuvalbenar <yuvalbenar@gmail.com>
2024-11-17 19:51:24 +0200

    these are the mid november python projects

--- firstexrecise.py
+++ firstexrecise.py
@@ -1,7 +1,15 @@
-capitals= {
-  'France': 'Paris',
-  'Spain': 'Madrid',
-  'Japan': 'Tokyo'
-}
-x = capitals.get("germany")
-print(x)
+def count_characters(string):
+    character_count = {}
+    for char in string:
+        if char in character_count:
+            character_count[char] += 1
+        else:
+            character_count[char] = 1
+    return character_count
+
+# Example usage
+input_string = "my oh my"
+result = count_characters(input_string)
+print(result) 
+
+
